fe733c975d36e01cf6742c5c5db17a0c346d0e35
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-07-04 12:43:33 +0530

    Initial checkin for Todo List
    Initial checkin for Todo List

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../Task';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  constructor() { }
+  public taskList: Task[] = [];
+  ngOnInit(): void {
+  }
+
+  addTask(): void {
+    this.taskList.push(new Task());
+  }
+
+  removeTask(index: number) {
+    if (index > -1) {
+      this.taskList.splice(index, 1);
+    }
+  }
+
+}
